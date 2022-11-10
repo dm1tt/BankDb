712fcca48074aa271cda8957175636db82ac04a9
Dmitry <Diman_krasnov02@mail.ru>
2022-11-10 03:09:56 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Npgsql;
+using System.Data;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestConection();
+            Console.ReadKey();
+        }
+        private static void TestConection()
+        {
+            using(NpgsqlConnection con = GetConnection())
+            {
+                con.Open();
+                if(con.State== ConnectionState.Open)
+                {
+                    Console.WriteLine("Connected");
+                }
+            }
+        }
+        private static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=localhost;Port=5433;User Id=postgres;Password=1234;Database=BankDirectory;");
+        }
+    }
+}
