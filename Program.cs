3674fdf3a4575f38faa5a524fe4ac514d15ce19f
Dmitry <Diman_krasnov02@mail.ru>
2022-11-14 05:58:48 +0300

    Isnert Query

--- Program.cs
+++ Program.cs
@@ -1,30 +1,46 @@
 ﻿using Npgsql;
+using System.Collections.Concurrent;
 using System.Data;
+using System.Diagnostics.Metrics;
 using System.Reflection.Metadata.Ecma335;
+using System.Xml.Serialization;
 
 namespace Bank
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static int choice()
         {
-            TestConection();
+            Console.WriteLine("Выберите вариант ответа:");
+            Console.WriteLine("1. КОНЕЧНО ЖЕ ДА!!  2.НЕТ Я ЧМО((");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Здарова мужик! Предлагаю тебе взять кредит в нашем прекраснм и мега популярном БДСМ банке!!");
             Console.ReadKey();
-        }
-        private static void TestConection()
-        {
-            using(NpgsqlConnection con = GetConnection())
+            Console.WriteLine("Полученные деньги ты впарве тратить на абсолютно любые приколы штуки для секса и всякие разные сладости из пятерочки!!!");
+            Console.ReadKey();
+            Console.WriteLine("Ну что? Приступаем к оформлению кредита?");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            while (choice() == 2) 
             {
-                con.Open();
-                if(con.State== ConnectionState.Open)
-                {
-                    Console.WriteLine("Connected");
-                }
-            }
-        }
-        private static NpgsqlConnection GetConnection()
-        {
-            return new NpgsqlConnection(@"Server=localhost;Port=5433;User Id=postgres;Password=1234;Database=BankDirectory;");
-        }
+                choice();
+            }            
+
+            string fullName = Console.ReadLine();
+            string phone = Console.ReadLine();
+            string city = Console.ReadLine();
+            string country = Console.ReadLine();
+            string reqs = Console.ReadLine(); 
+            QueryTool user = new QueryTool();
+            user.GeneralInsertQuery(fullName, phone, city, country, reqs);
+            Console.WriteLine(")))");
+            Console.ReadKey();
+        }       
+
     }
 }
---
+++ QueryTool.cs
@@ -0,0 +1,68 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class QueryTool
+    {
+        private static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=localhost;Port=5433;User Id=postgres;Password=1234;Database=BankDirectory;");
+        }
+
+        private static void InsertUpdateDelete(string insertQuery)
+        {
+           NpgsqlConnection con = GetConnection();
+           NpgsqlCommand insert = new(insertQuery, con);
+           con.Open();
+           insert.ExecuteNonQuery();
+           con.Close();
+        }
+        
+        private static string Select(string selectQuery)
+        {
+            NpgsqlConnection con = GetConnection();
+            NpgsqlCommand select = new(selectQuery, con);
+            con.Open();
+            string resultOfQuery = select.ExecuteScalar().ToString();
+            con.Close();
+            return resultOfQuery;
+        }
+
+        private static bool SelectExists(string selectExistsQuery)
+        {
+            NpgsqlConnection con = GetConnection();
+            NpgsqlCommand select = new(selectExistsQuery, con);
+            con.Open();
+            bool resultOfQuery = (bool)select.ExecuteScalar();
+            con.Close();
+            return resultOfQuery;
+        }
+
+        public void GeneralInsertQuery(string fullName, string telephone, string city, string country, string requisites)
+        {
+            if (SelectExists(@"select exists (select * from ""Residence"" where ""City"" = '"+city+"' )"))
+            {
+                InsertUpdateDelete(@"insert into ""Client""(""FullName"", ""CityId"", ""Requisites"") values ('" + fullName + "', '" + Select(@"select ""CityId"" from ""Residence"" where ""City"" = '" + city + "';") + "', '" + requisites + "')");
+            }
+            else
+            {
+                InsertUpdateDelete(@"insert into ""Residence"" (""City"", ""Country"") values ('" + city + "', '" + country + "')");
+
+                InsertUpdateDelete(@"insert into ""Client""(""FullName"", ""CityId"", ""Requisites"") values ('" + fullName + "', '" + Select(@"select ""CityId"" from ""Residence"" where ""City"" = '" + city + "';") + "', '" + requisites + "')");
+            } 
+            
+            InsertUpdateDelete(@"insert into ""Telephone"" (""UserId"", ""Phone"") values ('" + Select(@"select ""UserId"" from ""Client"" where ""FullName"" = '" + fullName + "'") + "', '" + telephone + "')");  
+        }
+
+        public void GeneralDeleteQuery(string fullName, string telephone, string city, string country, string requisites)
+        {
+
+        }
+    }
+}
